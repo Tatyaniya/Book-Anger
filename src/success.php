0a690a64cc9280bb0d9917dddb4ddaf5861e7833
Tatyaniya <afromyak13@gmail.com>
2020-10-25 23:17:00 +0300

    fix gif safari

--- src/success.php
+++ src/success.php
@@ -9,7 +9,7 @@ $text = htmlspecialchars($_POST["text"]);
 $refferer = getenv('HTTP_REFERER');
 $date=date("d.m.y"); // число.месяц.год  
 $time=date("H:i"); // часы:минуты:секунды 
-$myemail = "tatyaniya@bigmir.net"; // e-mail администратора
+$myemail = "mail@gmail.com"; // e-mail администратора
 
 
 // Отправка письма администратору сайта
@@ -22,14 +22,14 @@ $message_to_myemail = "Текст письма:
 Вопрос: $text<br>
 ";
 
-mail($myemail, $tema, $message_to_myemail, "From: potapov-gnev <afromyak13@gmail.com> \r\n Reply-To: potapov \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
+mail($myemail, $tema, $message_to_myemail, "From: potapov-gnev <mail@gmail.com> \r\n Reply-To: potapov \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
 
 
 // Сохранение инфо о лидах в файл leads.xls
 
 $f = fopen("leads.xls", "a+");
 fwrite($f," <tr>");    
-fwrite($f," <td>$name</td> <td>$tel</td>   <td>$date / $time</td>");   
+fwrite($f," <td>$name</td> <td>$mail</td>  <td>$text</td> <td>$date / $time</td>");   
 fwrite($f," <td>$refferer</td>");    
 fwrite($f," </tr>");  
 fwrite($f,"\n ");    
